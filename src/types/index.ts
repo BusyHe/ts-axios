aba54d23687fd8eff7ea9fc28370eb1541acb619
busyhe <525118368@qq.com>
2021-01-13 19:21:08 +0800

    add

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export type Method = 'get' | 'GET' | 'delete' | 'DELETE' | 'head' | 'HEAD' | 'post' | 'POST' | 'options' | 'OPTIONS' | 'put' | 'PUT' | 'patch' | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: String,
+  method?: Method
+}
